7a90e68f94085b3ee66eba42bcfb1582d72fc262
piyush <piyushbhele1995@gmail.com>
2023-01-03 00:46:18 +0530

    manual authentication added

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,10 +1,22 @@
 //import model
 const User = require('../models/user');
+const { use } = require('../routes/users');
 
 module.exports.profile = function (req, res) {
-    return res.render('profile', {
-        title: 'User Profile'
-    })
+    if (req.cookies.user_id) {
+        User.findById(req.cookies.user_id, function (err, user) {
+            if (user) {
+                return res.render('profile', {
+                    title: 'User Profile',
+                    user: user
+                })
+            }
+
+            return res.redirect('/sign-in');
+        });
+    } else {
+        return res.redirect('/sign-in');
+    }
 }
 
 //render the sign up page
@@ -50,6 +62,34 @@ module.exports.create = function (req, res) {
     })
 }
 
+
+// Sign in and create session for the user
 module.exports.createSession = function (req, res) {
+    //steps to authenticate
+    //find the user
+
+    User.findOne({ email: req.body.email }, function (err, user) {
+        //handle password that don't match
+        if (err) {
+            console.log('Error in finding user in signing in');
+            return;
+        }
+
+
+        if (user) {
+            //handle password that dosen't match
+            if (user.password != req.body.password) {
+                return res.redirect('back');
+            }
+
+            //handle session creation
+            res.cookie('user_id', user.id);
+            return res.redirect('/profile');
+
+        } else {
+            //handle user not found
+            return res.redirect('back');
+        }
+    })
 
 }
