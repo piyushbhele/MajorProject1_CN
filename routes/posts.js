26e18e4e18063ec6d782b6449a1ac91ac7d6bc5a
piyush <piyushbhele1995@gmail.com>
2023-01-07 23:29:24 +0530

    authentication to posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const postsController = require('../controllers/posts_controller');
 
-router.get('/posts', postsController.posts);
+router.get('/posts', passport.checkAuthentication, postsController.posts);
 router.post('/create-post', postsController.create);
 module.exports = router;
